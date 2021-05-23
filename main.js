04ef9b6aed61a03fab5479608154f0a7a6f2e926
Ishan Sahu <ishan20015.s@gmail.com>
2021-05-23 20:22:30 +0530

    Fully done

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const playerScore = document.getElementById("playerScore");
+const computerScore = document.getElementById("computerScore");
+const computerChoice = document.getElementById("computerChoice");
+const resetbtn = document.getElementById("btn");
+const resetButton = document.getElementById("button");
+const body = document.querySelector("body");
+const newh3 = document.createElement("h3");
+newh3.style.textAlign = "center";
+const collection = document.querySelector("#userCollection");
+body.insertBefore(newh3, collection);
+computerChoice.style.height = "200px";
+computerChoice.style.width = "200px";
+var infinitePlay = false;
 // computer random selection
 computerPlay = () => {
   const game = ["Rock", "Paper", "Scissor"];
@@ -14,9 +27,43 @@ var computer = 0;
 const option1 = document.getElementById("userCollection");
 option1.addEventListener("click", (e) => {
   console.log(e.target);
-
   round(e.target.alt, computerPlay);
+  displayScore(); //displaying player and computer score on screen
 });
+
+//all display material
+displayScore = () => {
+  if (computerPlay().toLowerCase() == "paper") {
+    computerChoice.style.backgroundImage = "url('./images/paper.png ')";
+    computerChoice.style.backgroundSize = "cover";
+  } else if (computerPlay().toLowerCase() == "rock") {
+    computerChoice.style.backgroundImage = "url('./images/rock.png ')";
+    computerChoice.style.backgroundSize = "cover";
+  } else {
+    computerChoice.style.backgroundImage = "url('./images/scissor.png ')";
+    computerChoice.style.backgroundSize = "cover";
+  }
+
+  playerScore.innerText = "Player Score is " + player;
+  computerScore.innerText = "Computer Score is " + computer;
+  if (infinitePlay == false) {
+    if (player == 5) {
+      alert(
+        "Player has won \n" +
+          "Click on the Reset button to reset the score or ignore to play indefinitely"
+      );
+      infinitePlay = true;
+      resetbtn.style.display = "flex";
+    } else if (computer == 5) {
+      alert(
+        "Computer has won \n" +
+          "Click on the Reset button to reset the score or ignore to play indefinitely"
+      );
+      resetbtn.style.display = "flex";
+      infinitePlay = true;
+    }
+  }
+};
 
 // algo for checking who wins
 round = (playerSelection, computerSelection) => {
@@ -27,38 +74,21 @@ round = (playerSelection, computerSelection) => {
     (playerSelection == "rock" && computerSelection == "scissor") ||
     (playerSelection == "scissor" && computerSelection == "paper")
   ) {
-    console.log('computer selection is "' + computerSelection + '"');
-    console.log('player selection is "' + playerSelection + '"');
-    console.log(
-      '"' +
-        playerSelection +
-        '" beats "' +
-        computerSelection +
-        '" hence player won'
-    );
+    newh3.textContent = "Player has won the round";
+
     player++;
-    console.log('player point is "' + player + '" ');
-    console.log('computer points is "' + computer + '" ');
+    c;
   } else if (
     (playerSelection == "rock" && computerSelection == "paper") ||
     (playerSelection == "scissor" && computerSelection == "rock") ||
     (playerSelection == "paper" && computerSelection == "scissor")
   ) {
-    console.log('computer selection is "' + computerSelection + '" ');
-    console.log('player selection is "' + playerSelection + '" ');
-    console.log(
-      '"' +
-        computerSelection +
-        '" beats "' +
-        playerSelection +
-        '" hence computer won'
-    );
+    newh3.textContent = "Computer has won the round";
+
     computer++;
-    console.log('player point is "' + player + '" ');
-    console.log('computer points is "' + computer + '" ');
   } else if (
-    playerSelection !== "paper" ||
-    playerSelection !== "rock" ||
+    playerSelection !== "paper" &&
+    playerSelection !== "rock" &&
     playerSelection !== "scissor"
   ) {
     console.log(playerSelection);
@@ -66,7 +96,18 @@ round = (playerSelection, computerSelection) => {
   } else {
     console.log(computerSelection);
 
-    console.log("It's a DRAW");
+    newh3.textContent = "It's a draw";
   }
   return 0;
 };
+
+//Reset the scores
+resetButton.addEventListener("click", () => {
+  player = 0;
+  computer = 0;
+  playerScore.innerText = "";
+  computerScore.innerText = "";
+  newh3.textContent = "";
+  infinitePlay = false;
+  resetbtn.style.display = "none";
+});
